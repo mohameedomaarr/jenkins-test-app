5f4dd51a8ca159fbd7179fb2d9d489fbf7f27b1a
Ubuntu <ubuntu@ip-172-31-32-199.ec2.internal>
2025-08-13 22:46:59 +0000

    Initial commit

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+set -e
 
-echo "Setting up virtual environmet"
+# Remove old venv if it exists
+rm -rf venv
+
+echo "Setting up virtual environment...Final"
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Upgrading pip..."
+pip install --upgrade pip
+
+echo "Installing pytest..."
+pip install pytest
+
+echo "Running tests..."
+pytest
+
